5ced6c3919b7531267ef4b20d7a12ea8c219b4bb
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-27 20:21:40 -0500

    Create NumeroJABA.java

---
+++ numerosprimosfibonacci/src/main/java/com/example/numerosprimosfibonacci/modelojaba/NumeroJABA.java
@@ -0,0 +1,12 @@
+package com.example.numerosprimosfibonacci.modelojaba;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class NumeroJABA {
+    private Long id_numero_jaba;
+    private int valornumero_jaba;
+    
+}
