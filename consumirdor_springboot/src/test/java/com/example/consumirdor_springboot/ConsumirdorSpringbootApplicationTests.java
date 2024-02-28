5aedbc1bf7bac4d1c81c6c01542446259d4fbaf0
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-27 21:25:56 -0500

    Create Consumidor

---
+++ consumirdor_springboot/src/test/java/com/example/consumirdor_springboot/ConsumirdorSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumirdor_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumirdorSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
