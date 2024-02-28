fa7b26d51593a17cd774146ed7a20227107c103f
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-27 20:27:36 -0500

    Update ConfigColasJABA.java

--- numerosprimosfibonacci/src/main/java/com/example/numerosprimosfibonacci/configuracionjaba/ConfigColasJABA.java
+++ numerosprimosfibonacci/src/main/java/com/example/numerosprimosfibonacci/configuracionjaba/ConfigColasJABA.java
@@ -1,7 +1,11 @@
 package com.example.numerosprimosfibonacci.configuracionjaba;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 @Configuration
 public class ConfigColasJABA {
     public static final String cola_primos="JABA_PRIMOS";
@@ -10,5 +14,11 @@ public class ConfigColasJABA {
     //DEFINIR LAS RUTAS
     public static final String ROUTINGA_JABA= "jaba-primos";
     public static final String ROUTINGB_JABA= "jaba_fibonacci";
+
+    //definir las colas
+    @Bean
+    public Queue colanumprimos(){return new Queue(cola_primos,false);}
+    @Bean
+    public Queue colafibonacci(){return new Queue(cola_fibonacci_jaba, false);}
     
 }
