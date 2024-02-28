4dcb1ac54ee46b34766a131d1a35249d097be967
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-27 20:11:08 -0500

    create

---
+++ numerosprimosfibonacci/src/main/java/com/example/numerosprimosfibonacci/NumerosprimosfibonacciApplication.java
@@ -0,0 +1,13 @@
+package com.example.numerosprimosfibonacci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumerosprimosfibonacciApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumerosprimosfibonacciApplication.class, args);
+    }
+
+}
