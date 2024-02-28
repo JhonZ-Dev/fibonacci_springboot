65546ff1060831663cf7ea90178f75edf1210c1c
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-27 21:48:22 -0500

    Finalizado

--- consumirdor_springboot/src/main/java/com/example/consumirdor_springboot/modelojaba/NumerosModelJaba.java
+++ consumirdor_springboot/src/main/java/com/example/consumirdor_springboot/modelojaba/NumerosModelJaba.java
@@ -1,5 +1,6 @@
 package com.example.consumirdor_springboot.modelojaba;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -8,9 +9,17 @@ import lombok.ToString;
 
 @Data
 @ToString
+@Entity
 public class NumerosModelJaba {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int numero_jaba;
+
+    public NumerosModelJaba(){
+
+    }
+    public NumerosModelJaba(int numero_jaba) {
+        this.numero_jaba = numero_jaba;
+    }
 }
