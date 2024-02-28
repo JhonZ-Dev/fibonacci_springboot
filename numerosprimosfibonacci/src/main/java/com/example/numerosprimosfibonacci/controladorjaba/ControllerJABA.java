01efb804fe26b6762deb86de6d0a63d0d7bb8592
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-27 20:32:57 -0500

    Update ControllerJABA.java

--- numerosprimosfibonacci/src/main/java/com/example/numerosprimosfibonacci/controladorjaba/ControllerJABA.java
+++ numerosprimosfibonacci/src/main/java/com/example/numerosprimosfibonacci/controladorjaba/ControllerJABA.java
@@ -1,4 +1,17 @@
 package com.example.numerosprimosfibonacci.controladorjaba;
 
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/colas_jaba")
 public class ControllerJABA {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    @Autowired
+    private DirectExchange exchange;
+    
 }
