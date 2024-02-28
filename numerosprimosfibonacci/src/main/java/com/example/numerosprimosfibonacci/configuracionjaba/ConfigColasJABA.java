6ac4dbb72091a907004788af5546d977b780c272
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-27 20:25:48 -0500

    Update ConfigColasJABA.java

--- numerosprimosfibonacci/src/main/java/com/example/numerosprimosfibonacci/configuracionjaba/ConfigColasJABA.java
+++ numerosprimosfibonacci/src/main/java/com/example/numerosprimosfibonacci/configuracionjaba/ConfigColasJABA.java
@@ -7,5 +7,8 @@ public class ConfigColasJABA {
     public static final String cola_primos="JABA_PRIMOS";
     public static final String cola_fibonacci_jaba = "JABA_FIBONACCI";
 
+    //DEFINIR LAS RUTAS
+    public static final String ROUTINGA_JABA= "jaba-primos";
+    public static final String ROUTINGB_JABA= "jaba_fibonacci";
     
 }
