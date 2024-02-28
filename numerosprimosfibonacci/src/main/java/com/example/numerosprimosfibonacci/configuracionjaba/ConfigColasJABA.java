009d608f368b848a53aeda58a5964910a404c69a
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-27 20:24:32 -0500

    Update ConfigColasJABA.java

--- numerosprimosfibonacci/src/main/java/com/example/numerosprimosfibonacci/configuracionjaba/ConfigColasJABA.java
+++ numerosprimosfibonacci/src/main/java/com/example/numerosprimosfibonacci/configuracionjaba/ConfigColasJABA.java
@@ -4,4 +4,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConfigColasJABA {
+    public static final String cola_primos="JABA_PRIMOS";
+    public static final String cola_fibonacci_jaba = "JABA_FIBONACCI";
+
+    
 }
